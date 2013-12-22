0d953fa2b8dd28347d6bd57d02cddb2d6eb81232
Haru2036 <haru2036@gmail.com>
2013-12-23 05:18:35 +0900

    関数名リネーム

--- nagato.hs
+++ nagato.hs
@@ -5,16 +5,16 @@ import Data.HashMap.Lazy
 
 searchAndCountWords :: String -> [String] -> Int
 getUnigramFrequency :: [String] -> HashMap String Int
-train :: String -> IO (HashMap String Int )
+trainClass :: String -> IO (HashMap String Int )
 
 searchAndCountWords key items = length $ Data.List.filter (==key) items
 getUnigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
 
-train inputString = do
+trainClass inputString = do
   mecab <- new2 "-Owakati"
   parseResult <- parse mecab inputString
   return $ getUnigramFrequency $ words parseResult 
 
 main = do
   line <- readLn
-  train line
+  trainClass line
