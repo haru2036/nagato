a86929a48383554a2381c6d10535b2aaaa9dfd32
Haru2036 <haru2036@gmail.com>
2014-01-20 20:38:08 +0900

    Complement Naive Bayes実装:分類も完了

--- NagatoIO.hs
+++ NagatoIO.hs
@@ -1,8 +1,9 @@
-module Nagato.NagatoIO
+module NagatoIO
 ( writeToFile
 , readFromFile
 , writeToFileCounts
 , readFromFileCounts
+, loadFileToClassify
 )where
 
 import System.IO
@@ -13,13 +14,19 @@ import Data.Map
 import Data.Serialize
 import Data.ByteString
 import Data.Either.Unwrap
+import System.IO.UTF8 as S
 
 
 writeToFile :: String -> [(String, Map String Float)] -> IO()
 readFromFile :: String -> IO [(String, Map String Float)]
 writeToFileCounts :: String -> [(String, Map String Int)] -> IO()
 readFromFileCounts :: String -> IO [(String, Map String Int)]
+loadFileToClassify :: String -> IO String
 
+loadFileToClassify fileName = do
+  handle <- openFile fileName ReadMode
+  contents <- S.hGetContents handle
+  return $ contents
 
 writeToFile filePath classes = do
   let bytes = Data.Serialize.encode classes
--- nagato_classify.hs
+++ nagato_classify.hs
@@ -1,39 +1,50 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 import System.IO
 import Control.Monad
 import Data.List
 import Data.Tuple
 import Data.Map
-import System.IO.UTF8 as S
 import qualified NagatoIO as NagatoIO 
 import qualified MeCabTools as MeCabTools
+import System.IO.UTF8 as S
 
 classify :: [(String, Float)] -> String
+classifyByComplementClasses :: [(String, Float)] -> String
 calcProbability :: [String] -> Map String Float -> Float
-loadFileToClassify :: String -> IO String
 makeProbabilityList :: [String] -> [(String, Map String Float)] -> [(String, Float)]
-
 lookupPropabilityOfWord :: String -> Map String Float -> Float
+classifyIO :: IO()
+classifyComplementIO :: IO()
 
 main = do
+  classifyIO
+  classifyComplementIO
+
+classifyIO = do
   classes <- NagatoIO.readFromFile "classes.bin"
-  readedSentence <- loadFileToClassify "toclassify.txt" 
+  readedSentence <- NagatoIO.loadFileToClassify "toclassify.txt" 
   S.putStrLn readedSentence
   parsedSentence <- MeCabTools.parseWakati readedSentence
   let propabilityList = makeProbabilityList (words parsedSentence) classes
   System.IO.print propabilityList
   System.IO.putStrLn $ classify propabilityList
 
+classifyComplementIO = do
+  classes <- NagatoIO.readFromFile "complementClasses.bin"
+  readedSentence <- NagatoIO.loadFileToClassify "toclassify.txt" 
+  S.putStrLn readedSentence
+  parsedSentence <- MeCabTools.parseWakati readedSentence
+  let propabilityList = makeProbabilityList (words parsedSentence) classes
+  System.IO.print propabilityList
+  System.IO.putStrLn $ classifyByComplementClasses propabilityList
+
 calcProbability words classMap = product $ Data.List.map (\a -> lookupPropabilityOfWord a classMap) words
 
 lookupPropabilityOfWord word classMap = maybe 1.0 (\a -> a + 1.0) (Data.Map.lookup word classMap)
 
-loadFileToClassify fileName = do
-  handle <- openFile fileName ReadMode
-  contents <- S.hGetContents handle
-  return $ contents
-
 makeProbabilityList words classes = Data.List.map (\a -> (fst a, calcProbability words (snd a))) classes 
 
-classify propabilityList =let valueList = snd (unzip propabilityList)
+classifyByComplementClasses propabilityList = let valueList = snd (unzip propabilityList)
+                          in maybe "error" (\a -> fst (propabilityList !! a)) $ (Data.List.elemIndex (minimum valueList)) valueList
+
+classify propabilityList = let valueList = snd (unzip propabilityList)
                           in maybe "error" (\a -> fst (propabilityList !! a)) $ (Data.List.elemIndex (maximum valueList)) valueList
