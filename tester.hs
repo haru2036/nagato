e9dbd108b2928864b191b69b094415735d480ba5
haru2036 <haru2036@gmail.com>
2014-02-14 00:07:58 +0900

    精度計測用の関数を定義した。まだ動くかわからない

---
+++ tester.hs
@@ -0,0 +1,40 @@
+import Text.CSV
+import Data.List
+import NagatoIO
+import qualified Nagato_classify as NC
+import MeCabTools as MeCabTools
+import Models
+
+
+main = do
+  csvEither <- parseCSVFromFile "testData.CSV" 
+  let csvData = either (\a-> []) (\a->a) csvEither
+  classes <- NagatoIO.readFromFile "classes.bin"
+  classesComplement <- NagatoIO.readFromFile "complementClasses.bin"
+  classed <- mapM (\x->test (head x) classes classesComplement) csvData
+  let compared = unzip $ map (\a -> judge a) $ zip [a !! 1 | a <- csvData] classed
+  let accuracyNormal = (realToFrac (length (filter (==True) (fst compared)))) / (realToFrac (length csvData))
+  let accuracyComplement = (realToFrac (length (filter (==True) (snd compared)))) / (realToFrac (length csvData))
+  putStrLn "normal:"
+  print accuracyNormal
+  putStrLn "complement:"
+  print accuracyComplement
+
+judge :: (String, (String, String)) -> (Bool, Bool)
+judge item = 
+  let trueAnswer = fst item
+      answers = snd item
+  in ((trueAnswer == (fst answers)),(trueAnswer == (snd answers )))
+
+test :: String -> [(String, Props)] -> [(String, Props)] -> IO(String, String)
+test text classNormal classComplement = do
+  wakati <- MeCabTools.parseWakati text
+  let wordList = words wakati
+  return ((doClassify wordList classNormal), (doClassifyComplement wordList classComplement))
+
+
+doClassify :: [String] -> [(String, Props)] -> String
+doClassify wordList props = NC.classify $ NC.makeProbabilityList wordList props
+
+doClassifyComplement :: [String] -> [(String, Props)] -> String
+doClassifyComplement wordList props = NC.classifyByComplementClasses $ NC.makeProbabilityList wordList props
