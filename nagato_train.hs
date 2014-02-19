b0c4093d5b6a1744ed9ab821f431e3113bbf64a2
haru2036 <haru2036@gmail.com>
2014-02-19 19:21:04 +0900

    余計な行を削除

--- nagato_train.hs
+++ nagato_train.hs
@@ -4,30 +4,16 @@ module Nagato_train
   ,calcParameterForClass
 )where
 import System.IO
-import Control.Monad
-import Text.MeCab
-import Text.JSON
 import Text.JSON.Generic
 import Data.List
-import Data.Tuple
 import Data.Map
-import Data.Serialize
 import Models
 import System.IO.UTF8 as S
 import NagatoIO as NagatoIO
 import MeCabTools as MeCabTools
 
-searchAndCountWords :: String -> [String] -> Int
-getUnigramFrequency :: [String] -> Freqs
-calcParameterForClass :: Freqs -> Int -> Props
 
-countFromSetting :: String -> IO [(String, Freqs)]
 
-trainFromSetting :: String -> IO [(String, Props)]
-trainClass :: String -> IO Freqs
-loadSettings :: String -> IO [(String, String)]
-loadClassStrings :: [String] -> IO [String]
-filenameToString :: String -> IO String
 
 data AClass = AClass {
   className :: String,
@@ -47,24 +33,30 @@ data FrequencyOfClasses = FrequencyOfClasses{
   }deriving(Eq, Show, Data, Typeable)
 
 
+searchAndCountWords :: String -> [String] -> Int
 searchAndCountWords key items = length $ Data.List.filter (==key) items
 
+getUnigramFrequency :: [String] -> Freqs
 getUnigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
 
+trainClass :: String -> IO Freqs
 trainClass inputString = do
   parseResult <- MeCabTools.parseFilteredChasenFormat inputString ["名詞"]
   return $ getUnigramFrequency $ parseResult 
 
+loadSettings :: String -> IO [(String, String)]
 loadSettings settingName = do
   handle <- openFile settingName ReadMode
   contents <- S.hGetContents handle
   return $ Data.List.map(\a -> (className a, dataSource a))(classes(decodeJSON contents :: ClassList))
 
+filenameToString :: String -> IO String
 filenameToString fileName = do
   handle <- openFile fileName ReadMode
   contents <- S.hGetContents handle
   return $ contents
 
+loadClassStrings :: [String] -> IO [String]
 loadClassStrings fileNames = do
   if length fileNames == 1
     then do
@@ -75,10 +67,12 @@ loadClassStrings fileNames = do
       deepStrs <- loadClassStrings $ drop 1 fileNames
       return $ str : deepStrs
 
+main :: IO()
 main = do
   trainResult <- trainFromSetting "classes.json"
   NagatoIO.writeToFile "classes.bin" trainResult
 
+trainFromSetting :: String -> IO [(String, Props)]
 trainFromSetting settingFileName = do
   classesList <- loadSettings settingFileName
   let unzippedClasses = unzip classesList
@@ -87,6 +81,7 @@ trainFromSetting settingFileName = do
   let classesTrained = Data.List.map (\a -> calcParameterForClass a 2) classesCounted
   return $ zip (fst unzippedClasses) classesTrained
 
+countFromSetting :: String -> IO [(String, Freqs)]
 countFromSetting settingFileName = do
   classesList <- loadSettings settingFileName
   let unzippedClasses = unzip classesList
@@ -94,4 +89,5 @@ countFromSetting settingFileName = do
   classesCounted <- mapM (\a -> trainClass a) classStrings
   return $ zip (fst unzippedClasses) classesCounted
 
+calcParameterForClass :: Freqs -> Int -> Props
 calcParameterForClass classMap alpha = Data.Map.map (\a -> ((realToFrac (a + 1) / (realToFrac ((sum (elems classMap) + (length (keys classMap)) * (alpha - 1))))))) classMap
