1a23e92cbeace83e9e63f192a93970d89dcc4002
haru2036 <haru2036@gmail.com>
2014-02-19 18:10:20 +0900

    型の宣言を移動した

--- MeCabTools.hs
+++ MeCabTools.hs
@@ -8,26 +8,17 @@ import Text.Regex
 import Text.MeCab
 import Data.List
 
-parseChasenFormat :: String -> IO String
-parseFilteredChasenFormat :: String -> [String] -> IO [String]
 parseWakati :: String -> IO String
-
-filterByPartOfSpeech :: String -> [String] -> [String]
-filterAPart :: String -> [String] -> Bool
-splitLine :: String -> String 
-doEachFilters :: String -> String -> Bool
-getSpeechFromSplitTab :: String -> String
-splitOnTab :: String -> [String]
-splitOnConma :: String -> [String]
-
 parseWakati sentence = do
   mecab <- new2 "-Owakati"
   parse mecab sentence
 
+parseChasenFormat :: String -> IO String
 parseChasenFormat sentence = do
   mecab <- new2 ""
   parse mecab sentence
 
+parseFilteredChasenFormat :: String -> [String] -> IO [String]
 parseFilteredChasenFormat sentence filters = do
   chasenString <- parseChasenFormat sentence
   let endStripped = init $ lines chasenString
@@ -35,22 +26,29 @@ parseFilteredChasenFormat sentence filters = do
   let returned = filterByPartOfSpeech strippedChasenString filters
   return returned
 
+splitOnTab :: String -> [String]
 splitOnTab line = splitRegex (mkRegex "\t") line
 
+splitOnConma :: String -> [String]
 splitOnConma line = splitRegex (mkRegex ",") line
 
+doEachFilters :: String -> String -> Bool
 doEachFilters part filtersItem = isPrefixOf part filtersItem
 
+filterAPart :: String -> [String] -> Bool
 filterAPart line filters = or $ map (\a -> doEachFilters (splitLine line) a) filters
 
+getSpeechFromSplitTab :: String -> String
 getSpeechFromSplitTab line = let splitted = splitOnTab line
   in if (length splitted) >= 2
     then splitted !! 1
     else ""
 
+splitLine :: String -> String 
 splitLine line = let splitted = splitOnConma (getSpeechFromSplitTab line)
   in if splitted /= []
     then head splitted
     else ""
 
+filterByPartOfSpeech :: String -> [String] -> [String]
 filterByPartOfSpeech chasenString filters = [head (splitOnTab x) | x <- (lines chasenString), (filterAPart x filters) == True]
